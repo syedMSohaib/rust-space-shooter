63e08fe9286462fb6c54e5e89c1682ea209ad027
Syed Muhammad Sohaib <33151569+syedMSohaib@users.noreply.github.com>
2022-03-21 17:13:26 +0500

    laser fire spwan by player added to system

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![allow(unused)] //silence unsed warning for learning 
 
+use bevy::ecs::system::Command;
 use bevy::prelude::*;
+use bevy::log;
 
 const WINDOW_TITLE: &str = "Space shooter game on Rust"; 
 const PLAYER_SPRITE: &str =  "Texture2D/shiptt_0012_Layer-3-copy-9.png";
 const BACKGROUND: &str = "Texture2D/bg1.png";
+const LASER_SPRITE: &str = "Texture2D/fireRay_copy.png";
+// const LASER_SPRITE: &str = "Texture2D/2_top.png";
 const TIME_STEP: f32 = 1. / 60.;
 pub struct Materials {
     background_materials: Handle<Image>,
-    player_materials : Handle<Image>
+    player_materials : Handle<Image>,
+    laser: Handle<Image>
 }   
 pub struct WinSize {
     w: f32,
@@ -19,8 +24,14 @@ pub struct WinSize {
 struct Player;
 
 #[derive(Component)]
-struct PlayerSpeed(f32);    //tuple struct
-impl Default for PlayerSpeed {
+struct Laser;
+
+#[derive(Component)]
+struct PlayerReadyToFire(bool);
+
+#[derive(Component)]
+struct Speed(f32);    //tuple struct
+impl Default for Speed {
     fn default() -> Self {
         Self(5.)
     }
@@ -40,6 +51,8 @@ fn main() {
         .add_startup_stage("game_setup_background", SystemStage::single(background_spawn.system()))
         .add_startup_stage("game_setup_player", SystemStage::single(player_spawn.system()))
         .add_system(player_movement.system())
+        .add_system(player_fire.system())
+        .add_system(laser_movement.system())
         .run()
 
 }
@@ -59,6 +72,7 @@ fn setup(
     commands.insert_resource(Materials {
         background_materials: asset_server.load(BACKGROUND),
         player_materials: asset_server.load(PLAYER_SPRITE),
+        laser: asset_server.load(LASER_SPRITE),
     });
 
     commands.insert_resource(WinSize {
@@ -87,13 +101,63 @@ fn player_spawn(mut commands: Commands, materials: Res<Materials>, winsize: Res<
         ..Default::default()
     })
     .insert(Player)
-    .insert(PlayerSpeed::default());   
+    .insert(PlayerReadyToFire(true))
+    .insert(Speed::default());   
 
 }
 
+fn laser_movement(
+    mut commands: Commands,
+    winsize: Res<WinSize>,
+    mut query: Query<(Entity, &Speed, &mut Transform, With<Laser>)>
+){
+    for(laser_entity, speed, mut transform, _) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.y += speed.0 + TIME_STEP;
+        
+        if translation.y > winsize.h {
+            commands.entity(laser_entity).despawn();
+        }
+    }
+}
+
+fn player_fire(
+    mut commands: Commands, 
+    materials: Res<Materials>,
+    keyboard_input: Res<Input<KeyCode>>, 
+    mut query: Query<(&Transform, &mut PlayerReadyToFire, With<Player>)>,
+){
+    for (mut transform, mut ready_to_fire, _) in query.iter_mut() {
+        if ready_to_fire.0 && keyboard_input.pressed(KeyCode::Space) {
+            let x = transform.translation.x;
+            let y = transform.translation.y; 
+            // info!("x={:?} y={:?}",x, y);
+            commands.spawn_bundle(SpriteBundle {
+                texture: materials.laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y + 15, 0.),
+                    scale: Vec3::new(0.1, 0.1, 1.0),
+                    ..Default::default() 
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(Speed::default());   
+
+            ready_to_fire.0 = false;
+        }
+
+        if keyboard_input.just_released(KeyCode::Space) {
+            ready_to_fire.0 = true;
+        }
+        
+    }   
+}
+
+
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>, 
-    mut query: Query<(&PlayerSpeed, &mut Transform, With<Player>)>,
+    mut query: Query<(&Speed, &mut Transform, With<Player>)>,
 ){
 
     for (speed, mut transform, _) in query.iter_mut() {
